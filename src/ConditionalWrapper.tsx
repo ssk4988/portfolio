4af29ab3adc5a425fda5c27bdfcd14e2970bc240
Sachin S <ssk4988@gmail.com>
2023-07-13 13:29:16 -0400

    change private setting

--- src/ConditionalWrapper.tsx
+++ src/ConditionalWrapper.tsx
@@ -1,3 +1,5 @@
+// https://blog.hackages.io/conditionally-wrap-an-element-in-react-a8b9a47fab2
+
 const ConditionalWrapper = ({ condition, wrapper, children }: any) => 
   condition ? wrapper(children) : children;
 
