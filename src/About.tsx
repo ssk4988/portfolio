2131140ef27c778ea6733cb39c70f0c336079e10
Sachin S <ssk4988@gmail.com>
2024-06-11 10:49:05 -0400

    update info and start using markdown

--- src/About.tsx
+++ src/About.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import Header from './Header';
 import './styles.css';
 import './About.css';
 import data from './assets/data.json';
 import pfp from './assets/linkedinpfp.jpg';
 import Education from './Education';
-import { Avatar, Container } from '@mui/material';
-
+import { Avatar, Container, IconButton, Link } from '@mui/material';
+import { GitHub, LinkOutlined, LinkedIn } from '@mui/icons-material';
 
 function About() {
+  const openLinkedIn = () => {
+    window.open('https://www.linkedin.com/in/sachin-sivakumar/');
+  }
+  const openGitHub = () => {
+    window.open('https://github.com/ssk4988/');
+  }
+  const openPortfolio = () => {
+    window.open('https://sachinsivakumar.netlify.app/');
+  }
   let educationList = data.Education.map(val => <Education key={val.name} data={val}></Education>);
   return (
     <Container component='main' maxWidth='xl' className='root-container'>
       <div className='description'>
         <Avatar alt="Sachin Sivakumar" src={pfp} className='pfp' sx={{ width: '5rem', height: '5rem' }} />
-        <p className='summary-statement'>Hi, I’m Sachin!
-          I’m an accomplished competitive programmer and open-source contributor with an interest in software development, artificial intelligence, and cybersecurity.
+        <p className='summary-statement'>Hi, I'm Sachin!
+          I'm a competitive programmer, problem solver, and open-source contributor with an interest in software development, artificial intelligence, and cybersecurity.
           This is a portfolio I made for showing off what I've done so far. Feel free to check out what I'm working on!
         </p>
       </div>
+      <Container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <IconButton onClick={openLinkedIn}>
+          <LinkedIn fontSize='large' />
+        </IconButton>
+        <IconButton onClick={openGitHub}>
+          <GitHub fontSize='large' />
+        </IconButton>
+        <IconButton onClick={openPortfolio}>
+          <LinkOutlined fontSize='large' />
+        </IconButton>
+      </Container>
       <h3>Education</h3>
       <div>
         {educationList}
--- src/Project.tsx
+++ src/Project.tsx
@@ -1,82 +1,48 @@
-import { Avatar, Button, Card, CardContent } from '@mui/material';
+import { Avatar, Card, CardContent, IconButton } from '@mui/material';
 import useImage from './useImage';
 import './styles.css';
 import data from './assets/data.json';
 import Tag from './Tag';
 import { useTheme } from "@mui/material";
+import ReactMarkdown from 'react-markdown';
+import { LinkOutlined } from '@mui/icons-material';
 
 function Project(props: any) {
     const logos = data.Logos as { [key: string]: string };
     const theme = useTheme();
     let { project } = props;
-    const { loading, error, image } = useImage(project.logo in logos ? logos[project.logo] : '');
-    let tags = project.tags.map((tag: string) => <Tag key={tag} data={tag}></Tag>);
+    const usingNameLogo = false; //project.namelogo && project.namelogo in logos;
+    let imgSrc = '';
+    if (usingNameLogo) {
+        imgSrc = logos[project.namelogo];
+    }
+    else if (project.logo && project.logo in logos) {
+        imgSrc = logos[project.logo];
+    }
+    const { image } = useImage(imgSrc);
+    const tags = project.tags.map((tag: string) => <Tag key={tag} data={tag}></Tag>);
+    const markdown = project.markdown ? <ReactMarkdown>{project.markdown.join("\n")}</ReactMarkdown> : null;
+    const openLink = () => {
+        window.open(project.link);
+    }
+    console.log(image);
+    const header = usingNameLogo ? <img src={image} style={{ height: '50px', width: 'auto' }} /> : <>
+        {imgSrc != '' && <Avatar src={image} sx={{ border: 2, borderColor: 'white' }} className='logo' />}
+        <h3>{project.name}</h3>
+        {project.link && <IconButton onClick={openLink}>
+            <LinkOutlined />
+        </IconButton>}
+    </>
     return (
         <Card className="card">
             <CardContent>
                 <div className='title-logo'>
-                    {project.logo && <Avatar src={image} sx={{ border: 2, borderColor: 'white' }} className='logo' />}
-                    <h3>{project.name}</h3>
+                    {header}
                 </div>
-                {project.description && 
-                    <i style={{color: theme.palette.text.secondary}} className='card-description'>{project.description}</i>
+                {project.description &&
+                    <i style={{ color: theme.palette.text.secondary }} className='card-description'>{project.description}</i>
                 }
-                {/* TODO: replace this with JSON Markdown */}
-                {(project.name as string).startsWith("Portfolio") &&
-                    <div>
-                        <ul>
-                            <li>Built from scratch</li>
-                        </ul>
-                    </div>
-                }
-                {(project.name as string).startsWith("Computer Vision") &&
-                    <div>
-                        <ul>
-                            <li>Currently experimenting with YOLOv4, a fast object detection model</li>
-                        </ul>
-                    </div>
-                }
-                {(project.name as string).startsWith("Platform Art") &&
-                    <div>
-                        <ul>
-                            <li>Visualizes Platform Art’s public art projects through an interactive map and directory</li>
-                            <li>Integrated with Firebase server to make content scalable and maintainable</li>
-                        </ul>
-                    </div>
-                }
-                {(project.name as string).startsWith("Blockchain") &&
-                    <div>
-                        <ul>
-                            <li>Evaluated both by simulation and math the probability of success of 51% attacks to rewrite the ledger using variable percentages of the total CPU Power</li>
-                        </ul>
-                    </div>
-                }
-                {(project.name as string).startsWith("ArcherX") &&
-                    <div>
-                        <ul>
-                            <li>Placed 7th at TSA Florida’s Game Design competition</li>
-                        </ul>
-                    </div>
-                }
-                {(project.name as string).startsWith("FBLA") &&
-                    <div>
-                        <ul>
-                            <li>Global leaderboard maintained through a Firebase server</li>
-                            <li>Settings allow for customized quizzes</li>
-                            <li>Coming to Google Play soon after being updated</li>
-                        </ul>
-                    </div>
-                }
-                {(project.name as string).startsWith("YouShop") &&
-                    <div>
-                        <ul>
-                            <li>Placed 7th at TSA Florida’s Software Development competition</li>
-                            <li>Firebase server allows to save user favorites</li>
-                            <li>Filters to narrow searches</li>
-                            <li>Coming to Google Play soon after being updated</li>
-                        </ul>
-                    </div>
-                }
+                {markdown}
                 <div className='row'>
                     {tags}
                 </div>
